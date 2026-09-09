0d9973d13f463f40ef1f093d4ddc1763d6fbe0a7
WarrenSCox <76867633+WarrenSCox@users.noreply.github.com>
2026-09-09 23:01:16 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE = 'mel-tom-wedding-v7.55';
+const CACHE = 'mel-tom-wedding-v7.56';
 
 const APP_SHELL = [
   './',
   './index.html',
-  './styles-v4.css?v=7.55',
-  './app-v4.js?v=7.55',
+  './styles-v4.css?v=7.56',
+  './app-v4.js?v=7.56',
   './config.js?v=6.8',
   './manifest.webmanifest',
   './icon-192.png',
